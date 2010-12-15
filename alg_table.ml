576d0fabb49b0f4a2c6bd8ae2fdd11c20625e838
Ethan Burns <burns.ethan@gmail.com>
2010-12-15 14:42:06 -0500

    Add a comment.

--- alg_table.ml
+++ alg_table.ml
@@ -2,6 +2,13 @@
     by strings Builds a table of algorithms for a given domain and allows
     lookup by strings.
 
+    NOTES: This is not quite right because it is just a table for
+    algorithms that accept Search.Domain modules.  There would need to
+    be multiple tables for different kinds of domains
+    (Search.Std_domain, etc.).  It would then be the job of the
+    domain's main function to concatinate the correct tables and
+    perform a lookup.
+
     @author eaburns
     @since 2010-12-15
 *)
