2a209866fdbf99b20431f0be0dfaca4daa168c01
Ethan Burns <burns.ethan@gmail.com>
2011-04-12 12:22:13 -0400

    Don't bother storing h in A* nodes since it can easily be recomputed from f and g.

--- astar.ml
+++ astar.ml
@@ -8,7 +8,6 @@ struct
 
   type node = {
     s : state;
-    h : float;
     mutable g : float;
     mutable f : float;
     mutable p : node;
@@ -27,8 +26,8 @@ struct
     a.pq_pos <- i
 
   let update_node opn ~node ~parent g =
+    node.f <- (node.f -. node.g) +. g;
     node.g <- g;
-    node.f <- node.h +. g;
     node.p <- parent;
     if node.pq_pos = no_pos then
       Dpq.insert opn node
@@ -43,18 +42,16 @@ struct
 	let n' = Ht.find cls s' in
 	if n'.g > g' then update_node opn ~node:n' ~parent:n g'
       with Not_found ->
-	let h = D.h s' in
 	let n' =
-	  { s = s'; p = n; h = h; g = g'; f = h +. g'; pq_pos = no_pos } in
+	  { s = s'; p = n; g = g'; f = D.h s' +. g'; pq_pos = no_pos } in
 	Dpq.insert opn n';
 	Ht.add cls s' n'
     in
     List.iter handle_child (D.succs ~parent:n.p.s ~state:n.s)
 
   let search _args state =
-    let h0 = D.h state in
     let rec init =
-      { s = state; p = init; h = h0; g = 0.; f = h0; pq_pos = no_pos } in
+      { s = state; p = init;g = 0.; f = D.h state; pq_pos = no_pos } in
     let opn = Dpq.create is_better update_pq_pos 1024 init in
     let cls = Ht.create 149 in
     let goal = ref None and expd = ref 0 and gend = ref 0 in
