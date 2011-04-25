18792e09a5c2a91d623f7ce60bfe0b6c773c03bb
Ethan Burns <burns.ethan@gmail.com>
2011-04-25 12:09:19 -0400

    Change Tiles to use a phantom type over the positions/contents array.

--- tiles/tiles.ml
+++ tiles/tiles.ml
@@ -2,21 +2,21 @@
 
 (* Disallow any confusion between positions and contents arrays. *)
 module Ary : sig
-  type contents = private string
+  type positions
+  type contents
+  type 'a ary = private string
 
-  type positions = private string
-
-  val positions : int -> positions
-  val contents : int -> contents
-  val set_pos : positions -> int -> int -> unit
-  val get_pos : positions -> int -> int
-  val set_cont : contents -> int -> int -> unit
-  val get_cont : contents -> int -> int
+  val positions : int -> positions ary
+  val contents : int -> contents ary
+  val set : 'a ary -> int -> int -> unit
+  val get : 'a ary -> int -> int
 
 end = struct
-  type contents = string
+  type contents
 
-  type positions = string
+  type positions
+
+  type 'a ary = string
 
   let positions size = String.create size
 
@@ -28,52 +28,47 @@ end = struct
 
   let get str ind =
     Char.code str.[ind]
-
-  let set_pos = set
-  let get_pos = get
-  let set_cont = set
-  let get_cont = get
 end
 
 open Ary
 
 type state = {
-  ary : contents;
+  ary : contents ary;
   h : float;
 }
 
 type inst = {
   rows : int;
   cols : int;
-  init : contents;
-  goal : positions;
+  init : contents ary;
+  goal : positions ary;
 }
 
 (** Create the canonical goal position set for each tile. *)
-let goal_pos : row:int -> col:int -> positions = fun ~row ~col ->
+let goal_pos : row:int -> col:int -> positions ary = fun ~row ~col ->
   let size = row * col in
   let pos = positions size in
-  for i = 0 to size - 1 do set_pos pos i i done;
+  for i = 0 to size - 1 do set pos i i done;
   pos
 
 let korf_12 =
   let s = contents 16 in
-  set_cont s 0 14;
-  set_cont s 1 1;
-  set_cont s 2 9;
-  set_cont s 3 6;
-  set_cont s 4 4;
-  set_cont s 5 8;
-  set_cont s 6 12;
-  set_cont s 7 5;
-  set_cont s 8 7;
-  set_cont s 9 2;
-  set_cont s 10 3;
-  set_cont s 11 0;
-  set_cont s 12 10;
-  set_cont s 13 11;
-  set_cont s 14 13;
-  set_cont s 15 15;
+  set s 0 14;
+  set s 1 1;
+  set s 2 9;
+  set s 3 6;
+  set s 4 4;
+  set s 5 8;
+  set s 6 12;
+  set s 7 5;
+  set s 8 7;
+  set s 9 2;
+  set s 10 3;
+  set s 11 0;
+  set s 12 10;
+  set s 13 11;
+  set s 14 13;
+  set s 15 15;
   { rows = 4;
     cols = 4;
     init = s;
@@ -90,7 +85,7 @@ let md_table inst =
     let locs = tab.(tile) in
     for loc = 0 to size - 1 do
       let srow = loc / rows and scol = loc mod rows in
-      let dloc = get_pos goal tile in
+      let dloc = get goal tile in
       let drow = dloc / rows and dcol = dloc mod rows in
       locs.(tile) <- abs (drow - srow) + abs (dcol - scol);
     done
@@ -103,8 +98,8 @@ let make_is_goal inst =
   let goal = inst.goal in
   let rec in_pos contents pos =
     if pos < size then begin
-      let tile = get_cont contents pos in
-      get_pos goal tile = pos && in_pos contents (pos + 1)
+      let tile = get contents pos in
+      get goal tile = pos && in_pos contents (pos + 1)
     end else
       true in
   (fun state -> in_pos state.contents 0)
