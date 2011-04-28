c59f9ffea3d89e88fef91c2a5ee1adf723254beb
Ethan Burns <burns.ethan@gmail.com>
2011-04-28 16:33:18 -0400

    Restructure the RTA* code to make it a bit simpler.

--- algs/rtastar.ml
+++ algs/rtastar.ml
@@ -1,87 +1,10 @@
-module type Eval = sig
-  type state
-
-  (** [eval stop depth_limit state] *)
-  val eval : (Info.t -> bool) -> Info.t -> int
-    -> parent:state -> state:state -> float
-end
+(* RTA*.  There are 2 versions: one uses in-place modification and one
+   uses out-of-place modification. *)
 
 let minf (a:float) b = if a < b then a else b
 
-(* Perform DFS using inplace modification. *)
-module Inplace_eval(D :
-  sig
-    include Search.Domain
-    include Search.Inplace with type inplace_state = state
-  end) : Eval with type state = D.state =
-struct
-  type state = D.state
-
-  let eval stop info dlim ~parent ~state =
-
-    let rec dfs ~alpha ~g depth gen_op state =
-      if D.is_goal state then
-	g
-      else begin
-	let f = g +. D.h state in
-	if depth < dlim && f < alpha && not (stop info) then begin
-	  let iter = D.succ_iter (Some gen_op) in
-	  info.Info.expd <- info.Info.expd + 1;
-	  best_kid ~alpha ~g (depth + 1) state iter
-	end else
-	  minf alpha f
-      end
-
-    and best_kid ~alpha ~g depth' state iter =
-      match D.next state iter with
-	| None -> alpha
-	| Some (c, op) ->
-	  info.Info.gend <- info.Info.gend + 1;
-	  let v = dfs ~alpha ~g:(g +. c) depth' op state in
-	  D.undo state op;
-	  best_kid ~alpha:(minf alpha v) ~g depth' state iter in
-
-    dfs ~alpha:infinity ~g:0. 0 (D.op parent state) state
-
-end
-
-(* Perform DFS using out-of-place evaluation of a state.  This can be
-   faster than the in-place version, especially in domains with small
-   state representations. *)
-module Outplace_eval(D : sig include Search.Domain end) :
-  Eval with type state = D.state =
-struct
-  type state = D.state
-
-  let eval stop info dlim ~parent ~state =
-
-    let rec dfs ~alpha ~g depth ~parent ~state =
-      if D.is_goal state then
-	g
-      else begin
-	let f = g +. D.h state in
-	if depth < dlim && f < alpha && not (stop info) then begin
-	  let succs = D.succs ~parent ~state in
-	  info.Info.expd <- info.Info.expd + 1;
-	  info.Info.gend <- info.Info.gend + (List.length succs);
-	  best_kid ~alpha ~g (depth + 1) state succs
-	end else
-	  minf alpha f
-      end
-
-    and best_kid ~alpha ~g depth' parent = function
-      | (k, c) :: ks ->
-	let v = dfs ~alpha ~g:(g +. c) depth' ~parent ~state:k in
-	best_kid ~alpha:(minf alpha v) ~g depth' parent ks
-      | [] -> alpha in
-
-    dfs ~alpha:infinity ~g:0. 0 ~parent ~state
-
-end
-
 (* The core of the RTA* search algorithm. *)
-module Make(E : Eval) (D : Search.Domain with type state = E.state) :
-  Search.Alg with type state = D.state =
+module Make (D : Search.Domain) =
 struct
   type state = D.state
 
@@ -120,17 +43,17 @@ struct
       end
     end
 
-  let h stop info dlim seen ~parent ~state =
+  let h eval stop info dlim seen ~parent ~state =
     try
       let h = Ht.find seen state in
       info.Info.dups <- info.Info.dups + 1;
       h
     with Not_found ->
-      let h = E.eval stop info dlim ~parent ~state in
+      let h = eval stop info dlim ~parent ~state in
       Ht.add seen state h;
       h
 
-  let search info lims args init =
+  let search eval info lims args init =
     let dlim = int_of_string args.(0) in
     if Array.length args > 1 then
       Random.init (int_of_string args.(1))
@@ -138,7 +61,7 @@ struct
       Random.self_init ();
     let stop = Limit.make_stop lims in
     let seen = Ht.create 149 in
-    let h = h stop info dlim seen in
+    let h = h eval stop info dlim seen in
     rtastar stop info h seen [init] 0. ~parent:init ~state:init
 end
 
@@ -147,7 +70,73 @@ module Inplace (D :
   sig
     include Search.Domain
     include Search.Inplace with type inplace_state = state
-  end) = Make(Inplace_eval(D))(D)
+  end) : Search.Alg with type state = D.state =
+struct
 
-(* The out-of-place (normal?) RTA* search module. *)
-module Outplace (D : Search.Domain) = Make(Outplace_eval(D))(D)
+  let eval stop info dlim ~parent ~state =
+
+    let rec dfs ~alpha ~g depth gen_op state =
+      if D.is_goal state then
+	g
+      else begin
+	let f = g +. D.h state in
+	if depth < dlim && f < alpha && not (stop info) then begin
+	  let iter = D.succ_iter (Some gen_op) in
+	  info.Info.expd <- info.Info.expd + 1;
+	  best_kid ~alpha ~g (depth + 1) state iter
+	end else
+	  minf alpha f
+      end
+
+    and best_kid ~alpha ~g depth' state iter =
+      match D.next state iter with
+	| None -> alpha
+	| Some (c, op) ->
+	  info.Info.gend <- info.Info.gend + 1;
+	  let v = dfs ~alpha ~g:(g +. c) depth' op state in
+	  D.undo state op;
+	  best_kid ~alpha:(minf alpha v) ~g depth' state iter in
+
+    dfs ~alpha:infinity ~g:0. 0 (D.op parent state) state
+
+  module Rtastar = Make(D)
+  include Rtastar
+
+  let search = search eval
+
+end
+
+(* The out-of-place RTA* search module. *)
+module Outplace (D : Search.Domain) : Search.Alg with type state = D.state =
+struct
+
+  let eval stop info dlim ~parent ~state =
+
+    let rec dfs ~alpha ~g depth ~parent ~state =
+      if D.is_goal state then
+	g
+      else begin
+	let f = g +. D.h state in
+	if depth < dlim && f < alpha && not (stop info) then begin
+	  let succs = D.succs ~parent ~state in
+	  info.Info.expd <- info.Info.expd + 1;
+	  info.Info.gend <- info.Info.gend + (List.length succs);
+	  best_kid ~alpha ~g (depth + 1) state succs
+	end else
+	  minf alpha f
+      end
+
+    and best_kid ~alpha ~g depth' parent = function
+      | (k, c) :: ks ->
+	let v = dfs ~alpha ~g:(g +. c) depth' ~parent ~state:k in
+	best_kid ~alpha:(minf alpha v) ~g depth' parent ks
+      | [] -> alpha in
+
+    dfs ~alpha:infinity ~g:0. 0 ~parent ~state
+
+  module Rtastar = Make(D)
+  include Rtastar
+
+  let search = search eval
+
+end
