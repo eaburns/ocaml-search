65f6c874b31e0d1739721c8ba75af9ed82e61f65
Ethan Burns <burns.ethan@gmail.com>
2011-04-28 09:53:45 -0400

    Don't return a goal from eval.  The code is a *lot* simpler and it seems to be equally as fast to just return the best leaf value from eval and not return a goal (if one is found).

--- algs/rtastar.ml
+++ algs/rtastar.ml
@@ -1,10 +1,9 @@
 
 module type Eval = sig
   type state
-  type res = Goal of state list * float | Val of float
 
   (** [eval stop depth_limit state] *)
-  val eval : (Info.t -> bool) -> Info.t -> int -> state -> res
+  val eval : (Info.t -> bool) -> Info.t -> int -> state -> float
 end
 
 let minf (a:float) b = if a < b then a else b
@@ -18,9 +17,8 @@ module Inplace_eval(D :
   end) : Eval with type state = D.state =
 struct
   type state = D.state
-  type res = Goal of state list * float | Val of float
 
-  let eval _stop _info _dlim _state = Val nan
+  let eval _stop _info _dlim _state = nan
 end
 
 (* Perform DFS using non-inplace methods. *)
@@ -28,31 +26,26 @@ module Outplace_eval(D : sig include Search.Domain end) :
   Eval with type state = D.state =
 struct
   type state = D.state
-  type res = Goal of state list * float | Val of float
 
   let eval stop info dlim state =
 
     let rec dfs alpha g depth ~parent ~state =
       let f = g +. D.h state in
       if D.is_goal state then
-	Goal ([state], g)
+	g
       else if not (stop info) && depth < dlim && f < alpha then begin
 	let succs = D.succs ~parent ~state in
 	info.Info.expd <- info.Info.expd + 1;
 	info.Info.gend <- info.Info.gend + (List.length succs);
-	match best_kid alpha g (depth + 1) state succs with
-	  | Goal (path, cost) -> Goal (state :: path, cost)
-	  | vl -> vl
+	best_kid alpha g (depth + 1) state succs
       end else
-	Val (minf alpha f)
+	minf alpha f
 
     and best_kid alpha g depth' parent = function
       | (k, c) :: ks ->
-	begin match dfs alpha (g +. c) depth' ~parent ~state:k with
-	  | Goal _ as g -> g
-	  | Val v -> best_kid (minf alpha v) g depth' parent ks
-	end
-      | [] -> Val alpha in
+	let v = dfs alpha (g +. c) depth' ~parent ~state:k in
+	best_kid (minf alpha v) g depth' parent ks
+      | [] -> alpha in
 
     dfs infinity 0. 0 ~parent:state ~state
 
@@ -72,49 +65,33 @@ struct
     else if D.is_goal state then
       Some (List.rev (state :: path), cost)
     else begin
-      let kids = ref (D.succs ~parent:state ~state) in
+      let kids = D.succs ~parent:state ~state in
       info.Info.expd <- info.Info.expd + 1;
-      info.Info.gend <- info.Info.gend + (List.length !kids);
-      let goal = ref None in
-      let mins = ref [] and minvl = ref infinity in
-      let sndvl = ref infinity in
-      while !goal = None && !kids <> [] do
-	let (k, c) = List.hd !kids in
-	kids := List.tl !kids;
-	match h k with
-	  | E.Goal (p, cst) ->
-	    goal := Some (List.rev (p @ path), c +. cst +. cost)
-	  | E.Val h ->
-	    let v = h +. c in
-	    if v < !minvl then begin
-	      sndvl := !minvl;
-	      minvl := v;
-	      mins := [k, c];
-	    end else if v = !minvl then
-	      mins := (k, c) :: !mins
-	    else if v < !sndvl then
-	      sndvl := v
-      done;
-      match !goal with
-	| Some _ -> !goal
-	| None when !mins = [] -> None	(* dead-end *)
-	| None ->
-	  Ht.replace seen state !sndvl;
-	  let len = List.length !mins in
-	  let n = if len = 1 then 0 else Random.int len in
-	  let k, c = List.nth !mins n in
-	  rtastar stop info h seen (k :: path) (cost +. c) k
+      info.Info.gend <- info.Info.gend + (List.length kids);
+      let mins = ref [] and minvl = ref infinity and sndvl = ref infinity in
+      let consider_kid ((k, c) as kid) =
+	let v = h k +. c in
+	if v < !minvl then begin
+	  sndvl := !minvl; minvl := v; mins := [kid];
+	end else if v = !minvl then
+	  mins := kid :: !mins
+	else if v < !sndvl then
+	  sndvl := v in
+      List.iter consider_kid kids;
+      Ht.replace seen state !sndvl;
+      let len = List.length !mins in
+      let n = if len = 1 then 0 else Random.int len in
+      let k, c = List.nth !mins n in
+      rtastar stop info h seen (k :: path) (cost +. c) k
     end
 
   let h stop info dlim seen state =
     try
-      E.Val (Ht.find seen state)
+      Ht.find seen state
     with Not_found ->
-      match E.eval stop info dlim state with
-	| (E.Val v) as vl ->
-	  Ht.add seen state v;
-	  vl
-	| E.Goal _ as g -> g
+      let h = E.eval stop info dlim state in
+      Ht.add seen state h;
+      h
 
   let search info lims args init =
     let dlim = int_of_string args.(0) in
